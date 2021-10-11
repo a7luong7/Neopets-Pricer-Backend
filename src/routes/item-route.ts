c5198432bbc574c8912bc16715f3fa1301663cd7
a7luong7 <samcheung992@gmail.com>
2021-10-10 21:00:50 -0700

    added check for duplicate item names

--- src/routes/item-route.ts
+++ src/routes/item-route.ts
@@ -7,6 +7,18 @@ require('express-async-errors');
 
 const itemRouter = express.Router();
 
+const findDuplicates = (names:string[]) : string[] => {
+  const duplicates = <string[]>[];
+  const sortedNames = names.map((x) => x.trim().toUpperCase()).sort();
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < sortedNames.length - 1; i++) {
+    if (sortedNames[i] === sortedNames[i + 1]) {
+      duplicates.push(sortedNames[i]);
+    }
+  }
+  return [...new Set(duplicates)];
+};
+
 itemRouter.post('/search', async (req:express.Request, res:express.Response) => {
   const itemNamesFromReq:any = req.body.itemNames;
   const isBadRequest = !itemNamesFromReq
@@ -34,6 +46,12 @@ itemRouter.post('/', async (req:express.Request, res:express.Response) => {
   let insertRequest = <ItemInsertRequest[]>[];
   try {
     insertRequest = reqBody.map((x) => toItemInsertRequest(x));
+    const duplicateNames = findDuplicates(insertRequest.map((x) => x.name));
+    if (duplicateNames.length !== 0) {
+      return res.status(400).json({
+        error: `Duplicate item names found: ${duplicateNames.join(', ')}`,
+      });
+    }
   } catch (e:any) {
     return res.status(400).json({ error: e.message });
   }
