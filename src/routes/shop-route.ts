b75bc93a49759c00e8c95439b116bb59ceceeddb
a7luong7 <samcheung992@gmail.com>
2021-10-11 20:36:03 -0700

    added route for getting shops

--- src/routes/shop-route.ts
+++ src/routes/shop-route.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { getShops } from '../services/shop-service';
 
 const shopRouter = express.Router();
 
 shopRouter.get('/', async (req:express.Request, res:express.Response) => {
-  res.status(404).send('not implemented yet');
+  const shops = await getShops();
+  return res.status(200).json(shops);
+  // res.status(404).send('not implemented yet');
 });
 
 export default shopRouter;
--- src/services/shop-service.ts
+++ src/services/shop-service.ts
@@ -1,4 +1,14 @@
 import ShopModel from '../models/shop';
 import {
   GenericResponse,
+  Shop,
+  ShopDTO,
+  ShopInsertRequest,
 } from '../types';
+
+export const getShops = async () : Promise<Shop[]> => {
+  const shops:Shop[] = await ShopModel.find();
+  return shops;
+};
+
+export default {};
